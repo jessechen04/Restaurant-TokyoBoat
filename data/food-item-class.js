231f94e88f341ffc6605e8c0d77b111b92be8092
jessechen04 <chen.jesse04@gmail.com>
2024-06-01 13:48:09 -0400

    set up data files for cart and menu items

---
+++ data/food-item-class.js
@@ -0,0 +1,13 @@
+class FoodItem {
+    name;
+    price;
+    description;
+    image;
+
+    constructor(name, price, description, image) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+    }
+}
---
+++ data/menu.js
@@ -0,0 +1,7 @@
+let kimchi = kimchi = new FoodItem('Kimchi', 500, '', '');
+
+let appetizers = [
+    kimchi
+];
+
+export let menu = [];
