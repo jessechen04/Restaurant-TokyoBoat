6b07bc5be58aeb8cb0e0ea13334c47db2f025867
jessechen04 <chen.jesse04@gmail.com>
2024-06-04 23:12:45 -0400

    completed login

--- backend/login.js
+++ backend/login.js
@@ -1,13 +1,19 @@
-const express = require('express');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const encoder = bodyParser.urlencoded();
+
 const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({path: './.env'});
 
-const app = express();
-
 const home = path.join(__dirname, '..', 'home.html');
+const orderScreen = path.join(__dirname, '..', 'order-online.html');
+const usernameScreen = path.join(__dirname, 'sign-in-username.html');
+const passwordScreen = path.join(__dirname, 'sign-in-password.html');
 const stylesPagesPath = path.join(__dirname, '..', 'styles', 'pages');
 const stylesSharedPath = path.join(__dirname, '..', 'styles', 'shared');
 
@@ -15,21 +21,21 @@ const stylesSharedPath = path.join(__dirname, '..', 'styles', 'shared');
 app.use('/styles/pages', express.static(stylesPagesPath));
 app.use('/styles/shared', express.static(stylesSharedPath));
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'your_password',
+    password: '',
     database: 'nodejs-login',
     port: 3377
-});*/
+});
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
     port: 3377
-});
+});*/
 
 //app.set('view engine', 'hbs');
 
@@ -49,6 +55,32 @@ app.get('/', (req, res) => {
     res.sendFile(home);
 });
 
+app.get('/sign-in-username', (req, res) => {
+    res.sendFile(usernameScreen);
+});
+
+app.post('/', encoder, (req, res) => {
+    var emailInput = req.body.email;
+    var passwordInput = req.body.password;
+    db.query('select * from users where email = ? and password = ?', [emailInput, passwordInput] , (error, results, fields) => {
+        if (results.length > 0) {
+            //console.log('hi');
+            res.redirect('/sign-in-password');
+        } else {
+            res.redirect('/sign-in-username');
+        }
+        //res.end();
+    });
+});
+
+app.get('/sign-in-password', (req, res) => {
+    res.sendFile(passwordScreen);
+});
+
+app.get('/order-online', (req, res) => {
+    res.sendFile(orderScreen);
+});
+
 app.listen(5000, () => {
-    console.log('Server started on port 5000.')
+    console.log('Server started on port 5000.');
 });
