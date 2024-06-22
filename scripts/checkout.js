0c58ae7677b9bfb50857dc231349433ba11f2139
jessechen04 <chen.jesse04@gmail.com>
2024-06-21 20:00:07 -0400

    more styles for order online and checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import {cart, getCartFromLocalStorage} from '../data/cart.js';
 import {menu, fetchMenuCategories, menuCategories} from '../data/menu.js';
 
+const TAX_RATE = 0.06;
+
 getCartFromLocalStorage();
 
 let yourBagHTML = '';
@@ -9,7 +11,9 @@ console.log(cart);
 fetchMenuCategories()
     .then(() => {
         
-        console.log(menu);
+        let itemsTotal = 0;
+        let tax;
+        let orderTotal;
 
         cart.forEach(cartItem => {
             let currentItem;
@@ -28,12 +32,31 @@ fetchMenuCategories()
             <div class="bag-item">
                 <div class="item-name">${currentItem.itemName}</div>
                 <div class="item-price">${(currentItem.itemPriceCents / 100).toFixed(2)}</div>
-                <div class="item-quantity">Quantity: ${cartItem.count} <div class="remove-item">Remove</div></div>
+                <div class="item-quantity">Quantity: ${cartItem.count} 
+                    <div class="edit-item">Edit</div> 
+                    <div class="remove-item">Remove</div>
+                </div>
             </div>
             `
+
+            itemsTotal += parseFloat((currentItem.itemPriceCents / 100));
+
         });
 
         document.querySelector('.your-bag-items').innerHTML = yourBagHTML;
+
+        tax = parseFloat((itemsTotal * TAX_RATE));
+        orderTotal = itemsTotal + tax;
+        document.querySelector('.order-summary-box').innerHTML = 
+        `
+        <div class="order-summary-text">Order Summary</div>
+        <div class="item-total-flex"><div>Items Total:</div><div>$${itemsTotal.toFixed(2)}</div></div>
+        <div class="tax-flex"><div>Tax:</div><div>$${tax.toFixed(2)}</div></div>
+        <div class="order-total-flex"><div>Order Total:</div><div>$${orderTotal.toFixed(2)}</div></div>
+        <button class="place-order-button">
+            <a class="place-order-text">Place Your Order</a>
+        </button>
+        `
 
         //document.querySelector('.remove-item').addEventListener('click', () => {
 
